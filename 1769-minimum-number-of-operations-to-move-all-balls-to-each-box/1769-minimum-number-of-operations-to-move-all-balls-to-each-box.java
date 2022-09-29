afa06fb2348750f84c6120cdacec56724196e61c
Sanyam17-beep <84903641+Sanyam17-beep@users.noreply.github.com>
2022-09-29 13:14:54 +0530

    Time: 299 ms (35.88%), Space: 47.4 MB (66.09%) - LeetHub

--- 1769-minimum-number-of-operations-to-move-all-balls-to-each-box/1769-minimum-number-of-operations-to-move-all-balls-to-each-box.java
+++ 1769-minimum-number-of-operations-to-move-all-balls-to-each-box/1769-minimum-number-of-operations-to-move-all-balls-to-each-box.java
@@ -3,15 +3,11 @@ class Solution {
         int ans[]=new int[boxes.length()];
         for(int i=0;i<boxes.length();i++){
             int c=0;
-            for(int j=0;j<i;j++){
+            for(int j=0;j<boxes.length();j++){
                 if(boxes.charAt(j)=='1'){
                   c+=(Math.abs(i-j));  
                 }
-            }
-            for(int j=i+1;j<boxes.length();j++){
-                if(boxes.charAt(j)=='1'){
-                     c+=(Math.abs(i-j));
-                }
+            
             }
             ans[i]=c;
         }
