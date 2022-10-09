907a250b927fae97641febbb87cb54c34f5ab69e
Sanyam17-beep <84903641+Sanyam17-beep@users.noreply.github.com>
2022-10-09 10:47:37 +0530

    Time: 395 ms (25.00%), Space: 160.2 MB (25.00%) - LeetHub

---
+++ 2435-paths-in-matrix-whose-sum-is-divisible-by-k/2435-paths-in-matrix-whose-sum-is-divisible-by-k.java
@@ -0,0 +1,30 @@
+class Solution {
+    int mod=(int)1e9+7;
+    public int numberOfPaths(int[][] grid, int k) {
+        Integer[][][] dp=new Integer[grid.length+1][grid[0].length][50];
+      return paths(grid,k,0,0,grid.length-1,grid[0].length-1,0,dp);  
+    }
+     public int paths(int[][] grid, int k,int sr,int sc,int dr,int dc,int target,Integer[][][] dp) {
+          if(sr < 0 || sr == grid.length || sc < 0 || sc == grid[0].length) {
+            return 0;
+        }
+          target+=grid[sr][sc];
+              if(sr==dr&&sc==dc){
+          
+           if(target%k==0){
+               return 1;
+           }
+           else{
+               return 0;
+           }
+       }
+         if(dp[sr][sc][target%k]!=null){
+             return dp[sr][sc][target%k];
+         }
+        int count=0;
+         
+         count=paths(grid,k,sr+1,sc,dr,dc,target,dp)+paths(grid,k,sr,sc+1,dr,dc,target,dp);
+
+         return dp[sr][sc][target%k]=count%mod;
+    }
+}
